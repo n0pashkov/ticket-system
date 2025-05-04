b4f83ff5a4dae8f5083ed2e11e9a5c12e5641a05
n0pashkov <npaskov32@gmail.com>
2025-05-04 21:14:15 +0300

    Добавлено свойство disableRestoreFocus в компонент EquipmentManagementPage для улучшения управления фокусом в диалогах. Это изменение направлено на оптимизацию пользовательского опыта при взаимодействии с интерфейсом.

--- ticket-system-frontend/src/pages/EquipmentManagementPage.js
+++ ticket-system-frontend/src/pages/EquipmentManagementPage.js
@@ -904,6 +904,7 @@ const EquipmentManagementPage = () => {
           }
         }}
         fullScreen={window.innerWidth < 600}
+        disableRestoreFocus
       >
         <DialogTitle 
           sx={{ 
